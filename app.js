c6007d8359fe17792b00fff6294849e689b529cb
jose torres & austin blanchard <student@epicodus.com>
2015-09-23 14:28:48 -0700

    add route to index.html

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 var tacoBout = angular.module('tacoBout', ['ui.router']);
+
+tacoBout.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url:"",
+    templateUrl: "partials/home.html"
+  });
+});
