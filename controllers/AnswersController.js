dc57bf50f1a87d359d20a35690d1dcc1960c5de6
Jose Torres & Austin Blanchard <student@epicodus.com>
2015-09-24 15:48:20 -0700

    adds search filtering capabilities.

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -2,8 +2,9 @@ tacoBout.controller('AnswersCtrl', function AnswersCtrl($scope, $stateParams, Qu
   $scope.question = UtilitiesFactory.findById(QuestionsFactory.questions, $stateParams.questionId)
   $scope.addAnswer = function() {
     // console.log(answerName);
-    $scope.question.answers.push({ name: $scope.answerName });
+    $scope.question.answers.push({ name: $scope.answerName, questionAnswered: true });
     $scope.answerName = null;
+    $scope.question.answered = true;
     console.log($scope.question.answers[0]);
   }
 });
