5eaa1dba79c382edc32be68e09cc6b48ef494182
Jose Torres & Austin Blanchard <student@epicodus.com>
2015-09-24 12:18:53 -0700

    add hard-coded questions to serve as examples.

--- services/QuestionsFactory.js
+++ services/QuestionsFactory.js
@@ -1,6 +1,6 @@
 tacoBout.factory('QuestionsFactory', function QuestionsFactory() {
   var factory = {};
-  factory.questions = [ {name: 'What is a taco?', id: 1, answers: []}, {name: 'Corn vs flour?', id: 2, answers: []}, {name: 'Is nine tacos too many?', id: 3, answers: []} ];
+  factory.questions = [{ name: 'What is a taco?', id: 1, answers: [{ name: 'A snack sized bite of heaven.' }]}, { name: 'Corn vs flour?', id: 2, answers: [{ name: 'Corn. Always.' }]}, { name: 'Is nine tacos too many?', id: 3, answers: [{name: 'Nine is not as many as twelve.'}]}];
   factory.addQuestion = function() {
     factory.questions.push({ name: factory.questionName, id: factory.questions.length + 1, answers: [] });
     factory.questionName = null;
