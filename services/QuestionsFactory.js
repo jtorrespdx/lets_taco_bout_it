11fdf75e6c2505d0474b199f5e3e44191d2557c7
Jose Torres & Austin Blanchard <student@epicodus.com>
2015-09-24 12:06:57 -0700

    begins work to add search & submit functionality.

--- services/QuestionsFactory.js
+++ services/QuestionsFactory.js
@@ -1,6 +1,6 @@
 tacoBout.factory('QuestionsFactory', function QuestionsFactory() {
   var factory = {};
-  factory.questions = [];
+  factory.questions = [ {name: 'What is a taco?', id: 1, answers: []}, {name: 'Corn vs flour?', id: 2, answers: []}, {name: 'Is nine tacos too many?', id: 3, answers: []} ];
   factory.addQuestion = function() {
     factory.questions.push({ name: factory.questionName, id: factory.questions.length + 1, answers: [] });
     factory.questionName = null;
